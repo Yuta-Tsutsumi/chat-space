008fe3b5d0e2ee1d8fbe2eb3d3af31e0f5824ba3
YourName <you@example.com>
2020-05-25 11:03:16 +0900

    修正しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group
+
+
   def index
   end
 
@@ -18,11 +21,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
@@ -33,6 +34,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+  
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
